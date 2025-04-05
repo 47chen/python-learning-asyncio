f9a4a578016367d1c1a53f147a728a5df530bbdc
47chen <x841010@gmail.com>
2025-04-05 21:45:42 +0800

    use lock = asyncio.Lock() to synchronize thread 1. Run one Coroutine one at a time if there are multiple 2. Lock will check whether any other Coroutine is using the lock 3. If yes, it will going to wait until that Corouine is finished 4. If no, it will run the code block within async with lock: 5. await for asyncio.sleep(5) simulated IO bound 6. await for asyncio.gather schedules & run tasks

---
+++ asyncio-in-python-TechWithTime/lock.py
@@ -0,0 +1,62 @@
+import asyncio
+
+"""
+Tools that allow us to synchronize the multiple Coroutines
+- Lock
+
+We want to make sure that there are no 2 Coroutines are working on this at the same time.
+
+"""
+
+# A shared variable
+shared_resource = 0 # Maybe a table, files
+
+# An asyncio Lock
+lock = asyncio.Lock()
+
+async def modify_shared_resource():
+    global shared_resource
+
+    # what it will do is that - it will check "Whether any other Coroutine is using the LOCK"
+    # If it is, it will going to wait until that Coroutine is finished
+    # If it's not, it will run the following code block
+    async with lock: # <- Acquire the lock by async with lock, async context manager
+        # Critical section starts
+        print(f'Resource before modification: {shared_resource}')
+
+        shared_resource += 1 # Modify the shared resource
+        await asyncio.sleep(5) # Simulate an IO operation
+        
+        print(f'Resource after modification: {shared_resource}')
+        print('='*40)
+        # Critical session ends
+
+
+async def main():
+    await asyncio.gather(*(modify_shared_resource() for _ in range(5)))
+
+
+asyncio.run(main())
+
+
+        
+
+
+"""
+Resource before modification: 0 
+Resource after modification: 1 - wait 5 seconds
+========================================
+Resource before modification: 1
+Resource after modification: 2 - wait 5 seconds
+========================================
+Resource before modification: 2
+Resource after modification: 3 - wait 5 seconds
+========================================
+Resource before modification: 3
+Resource after modification: 4 - wait 5 seconds
+========================================
+Resource before modification: 4
+Resource after modification: 5 - wait 5 seconds
+========================================
+"""
+
